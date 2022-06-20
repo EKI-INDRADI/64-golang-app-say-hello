1c518ddff8b48e360a7fe7c8bb4d2ba4c06ec512
EKI-INDRADI <eki.indradi@gmail.com>
2022-06-20 12:22:16 +0700

    major update

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/EKI-INDRADI/64-golang-app-say-hello
 
 go 1.18
 
-require github.com/EKI-INDRADI/63-golang-modules-go-say-hello v1.5.0
+require github.com/EKI-INDRADI/63-golang-modules-go-say-hello/v2 v2.0.0 // indirect
--- main.go
+++ main.go
@@ -3,9 +3,9 @@ package main
 import (
 	"fmt"
 
-	go_say_hello "github.com/EKI-INDRADI/63-golang-modules-go-say-hello"
+	go_say_hello "github.com/EKI-INDRADI/63-golang-modules-go-say-hello/v2"
 )
 
 func main() {
-	fmt.Println(go_say_hello.SayHello())
+	fmt.Println(go_say_hello.SayHello("eki"))
 }
