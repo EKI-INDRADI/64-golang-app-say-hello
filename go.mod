6cf80a7248068e326d0eb557fcdea11fadf70acd
EKI-INDRADI <eki.indradi@gmail.com>
2022-06-20 11:36:31 +0700

    tambah depedency

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/EKI-INDRADI/64-golang-app-say-hello
+
+go 1.18
+
+require github.com/EKI-INDRADI/63-golang-modules-go-say-hello v1.0.0 // indirect
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	go_say_hello "github.com/EKI-INDRADI/63-golang-modules-go-say-hello"
+)
+
+func main() {
+	fmt.Println(go_say_hello.SayHello())
+}
